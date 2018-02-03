eb23493d96d653feafa3e455f7add6c232d193c2
Anthony <alam6@stuy.edu>
2018-02-03 12:05:01 -0500

    tested and added fib

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -8,4 +8,20 @@ public class Recursion{
 		}
 		return n*fact(n-1);
 	}
+	public int fib(int n){
+		if (n<0){
+			throw new IllegalArgumentException();
+		}
+		return fibHelp(0,1,n);
+	}
+	public int fibHelp(int num1, int num2, int n){
+		if (n==1){
+			return num1;
+		}
+		return fibHelp(num1 +num2,num1,n-1);
+	}
+	public static void main(String[] args) {
+		Recursion a =new Recursion();
+		System.out.println(a.fib(10));
+	}
 }
