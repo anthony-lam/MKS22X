3393edc5e6b0229b23fe29b4ccfd0e83e0844130
Anthony <alam6@stuy.edu>
2018-03-08 14:44:36 -0500

    fixed compile errors

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,46 +1,62 @@
+import java.util.*;
+import java.io.*;
 public class USACO{
     public static int bronze(String filename){
-	File text = new File(filename);
-      	Scanner inf = new Scanner(text);
-	int r = inf.nextInt();
-	int c = inf.nextInt();
-	int e = inf.nextInt();
-	int n = inf.nextInt();
-	int[][] field = new int[r][c];
-	for (int row=0; row<r;row++){
-	    for (int col=0; col<c; col++){
-		field[row][col]=inf.nextInt();
-	    }
-	}
-	while (inf.hasNextLine()){
-	    int R_s = inf.nextInt();
-	    int C_s = inf.nextInt();
-	    int D_s = inf.nextInt();
-	    int max=0;
-	    for (int r=0; r<3; r++){
-		for (int c=0; c<3; c++){
-		    if (field[R_s+r][C_s+c]>max){
-			max=field[R_s+r][C_s+c];
-		    }
-		}
-	    }
-	    int newValue= max-D_s;
-	    for (int r=0; r<3; r++){
-		for (int c=0; c<3; c++){
-		    if (field[R_s+r][C_s+c]>newValue){
-			field[R_s+r][C_s+c]=newValue;
-		    }
-		}
-	    }
-	}
 	int total=0;
-	for (int r=0; r<3; r++){
-	    for (int c=0; c<3; c++){
-		if (field[R_s+r][C_s+c]<e){
-		    total+=e-field[R_s+r][C_s+c];
+	try{
+	    File text = new File(filename);
+	    Scanner inf = new Scanner(text);
+	    int numRow = inf.nextInt();
+	    int numCol = inf.nextInt();
+	    int e = inf.nextInt();
+	    int n = inf.nextInt();
+	    int[][] field = new int[numRow][numCol];
+	    for (int row=0; row<numRow;row++){
+		for (int col=0; col<numCol; col++){
+		    field[row][col]=inf.nextInt();
 		}
 	    }
+	    while (inf.hasNextLine()){
+		int R = inf.nextInt();
+		int C = inf.nextInt();
+		int D_s = inf.nextInt();
+		int max=0;
+		for (int r=0; r<3; r++){
+		    for (int c=0; c<3; c++){
+			if (field[R+r][C+c]>max){
+			    max=field[R+r][C+c];
+			}
+		    }
+		}
+		int newValue= max-D_s;
+		for (int r=0; r<3; r++){
+		    for (int c=0; c<3; c++){
+			if (field[R+r][C+c]>newValue){
+			    field[R+r][C+c]=newValue;
+			}
+		    }
+		}
+	    }
+	    for (int r=0; r<numRow; r++){
+		for (int c=0; c<numCol; c++){
+		    if (field[r][c]<e){
+			total+=e-field[r][c];
+		    }
+		}
+	    }
+	}
+	catch(FileNotFoundException e){
+	    System.out.println("Error");
 	}
 	return total * 72 * 72;
+    }
+    public static int silver(String filename){
+	try{
+	    File text = new File(filename);
+	    Scanner inf = new Scanner(text);
+	}
+	catch(FileNotFoundException e){
+	}
+	return -1;
     }
 }
