d35fe09070dc41c968ce13552b6ffa010a91d413
Anthony <alam6@stuy.edu>
2018-03-13 20:59:47 -0400

    finished quickselect

--- 06/QuickSelect.java
+++ 06/QuickSelect.java
@@ -1,15 +1,14 @@
 import java.util.*;
 public class QuickSelect{
-	private int partition ( int [] data, int start, int end){
-		int pivot = data[(start+end)/2];
-		int pivotIndex = (start+end)/2;
+	private static int partition (int [] data, int start, int end){
+		int pivotIndex = (int)(Math.random()*(end-start)+start); 
+		int pivot = data[pivotIndex];
 		int small = start;
 		int large = end;
 		int temp = data [start];
 		data[start]=pivot;
 		data[pivotIndex]=temp;
-		System.out.println(pivot);
-		while (small != large-1){
+		while (small <= large){
 			if (data[small] > pivot){
 				temp = data[large];
 				data[large] = data[small];
@@ -20,13 +19,33 @@ public class QuickSelect{
 				small+=1;
 			}
 		}
-		data[small+1]=pivot;
+		temp = data[large];
+		data[large]=pivot;
+		data[start]=temp;
+		return large;
+	}
+	public static int quickselect(int []data, int k){
+  //return the value that is the kth smallest value of the array. 
+  //use your partition method to help you accomplish this.
+		int start = 0;
+		int end = data.length-1;
+		int i = partition(data,start,end);
+		while (i!=k-1){
+			if (i>k-1){
+				end = i ; 
+				i = partition(data, start, end);
+			}
+			else{
+				start = i;
+				i = partition(data, start, end);
+			}
+		}
 		System.out.println(Arrays.toString(data));
-		return small + 1;
+		return data[i];
 	}
 	public static void main(String[] args) {
 		QuickSelect a = new QuickSelect();
 		int[] test = {1,56,4,1,23,54,3,3535,54};
-		System.out.println(a.partition(test,0,8));
+		System.out.println(a.quickselect(test,4));
 	}
 }
