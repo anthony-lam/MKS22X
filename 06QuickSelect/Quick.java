206ee7b289701364dd5fa1d5013bf03c305fd145
Anthony <alam6@stuy.edu>
2018-03-19 10:11:28 -0400

    finished dutch

--- 06QuickSelect/Quick.java
+++ 06QuickSelect/Quick.java
@@ -1,89 +1,94 @@
 import java.util.*;
 public class Quick{
-    private static int partition (int [] data, int start, int end){
-	int pivotIndex = (int)(Math.random()*(end-start)+start); 
-	int pivot = data[pivotIndex];
-	int small = start;
-	int large = end;
-	int temp = data [start];
-	data[start]=pivot;
-	data[pivotIndex]=temp;
-	while (small <= large){
-	    if (data[small] > pivot){
+	private static int partition (int [] data, int start, int end){
+		int pivotIndex = (int)(Math.random()*(end-start)+start); 
+		int pivot = data[pivotIndex];
+		int small = start;
+		int large = end;
+		int temp = data [start];
+		data[start]=pivot;
+		data[pivotIndex]=temp;
+		while (small <= large){
+			if (data[small] > pivot){
+				temp = data[large];
+				data[large] = data[small];
+				data[small] = temp;
+				large-=1;
+			}
+			else{
+				small+=1;
+			}
+		}
 		temp = data[large];
-		data[large] = data[small];
-		data[small] = temp;
-		large-=1;
-	    }
-	    else{
-		small+=1;
-	    }
+		data[large]=pivot;
+		data[start]=temp;
+		return large;
 	}
-	temp = data[large];
-	data[large]=pivot;
-	data[start]=temp;
-	return large;
-    }
-    private static swap(int[] data, int x, int y){
-	int temp = data[x];
-	data[x] = data[y];
-	data[y] = temp;
-    }
-    private static int partition2 (int [] data, int start, int end){
-	int pivotIndex = (int)(Math.random()*(end-start)+start); 
-	int pivot = data[pivotIndex];
-	int small = start;
-	int large = end;
-	int i = start;
-	swap(data, start, pivotIndex);
-	while (i <= large){
-	    if (data[i] == pivot){
-		i++;
-	    }
-	    else{
-		if (data[i] > pivot){
-		    swap(data, i, large);
-		    large--;
-		}
-		else{
-		    swap(data, i, small);
-		    small++;
-		    i++;
-		}
-	    }
+	private static void swap(int[] data, int x, int y){
+		int temp = data[x];
+		data[x] = data[y];
+		data[y] = temp;
 	}
-	temp = data[large];
-	data[large]=pivot;
-	data[start]=temp;
-	return large;
-    }
-    public static int quickselect(int []data, int k){
+	public static int dutchPartition(int[] data, int start, int end){
+		int pivotIndex = (int)(Math.random()*(end-start)+start);
+		int pivot = data[pivotIndex];
+		int small = start;
+		int large = end;
+		int i = start;
+		int lt = start;
+		int gt = end;
+
+		swap(data,start,pivotIndex);
+
+		while (i <= gt){
+			if(data[i] == pivot){
+				i++;
+			}
+			else{
+				if(data[i] < pivot){
+					swap(data,lt,i);
+					lt++;
+					i++;
+				}
+				if(data[i] > pivot){
+					swap(data,gt,i);
+					gt--;
+				}
+			}
+		}
+		return lt;	
+	}
+
+	public static int quickselect(int []data, int k){
 	//return the value that is the kth smallest value of the array. 
 	//use your partition method to help you accomplish this.
-	int start = 0;
-	int end = data.length-1;
-	int i = end;
-	while (i!=k-1){
-	    if (i>k-1){
-		end = i ; 
-		i = partition(data, start, end);
-	    }
-	    else{
-		start = i;
-		i = partition(data, start, end);
-	    }
+		int start = 0;
+		int end = data.length;
+		int[] i = partition2(data,start,end);
+		k-=1;
+		while (!(k >= i[0] && k<= i[1]){
+			if (i[0]>k){
+				end = i[0] ; 
+				i = partition(data, start, end);
+			}
+			else{
+				start = i[1];
+				i = partition(data, start, end);
+			}
+		}
+		return data[i[0]];
 	}
-	System.out.println(Arrays.toString(data));
-	return data[i];
-    }
-    public static void quicksort(int[]ary){
-	
-    }
-    public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-        System.out.println(Quick.quickselect(arr, 9));
+	public static void quicksort(int[]ary){
+		quicksortH(ary, 0, ary.length-1);
+	}
+
+	private static void quicksortH(int[] data, int start, int end)
+	public static void main(String[] args) {
+		int[] arr = {1,23423,33,42,523,65,73,2,9};
+		quicksort(arr);
+		System.out.println(Arrays.toString(arr));
         // System.out.println(Arrays.toString(arr));
         // System.out.println(Quick.partition(arr, 0, arr.length - 1));
         // System.out.println(Arrays.toString(arr));
-    }
+	}
 }
