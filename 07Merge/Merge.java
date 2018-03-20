64c4f38c9431a6fb8526a56534a9a34a26747b6a
Anthony <alam6@stuy.edu>
2018-03-20 14:46:58 -0400

    added msort

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,6 +1,5 @@
 public class Merge{
-    private static void merge (int[] data, int[] temp, int lo, int hi){
-	int mid = lo/2 + hi/2;
+    private static void merge (int[] data, int[] temp, int lo, int mid, int hi){
 	for (int l=lo; lo<=hi; l++){
 	    if (lo < mid && (mid>hi || temp[lo] > temp[mid])){
 		data[l] = temp[mid];
@@ -10,4 +9,17 @@ public class Merge{
 	    }
 	}
     }
+    public static void mergeSort(int[] data){
+	int[] temp = new int[data.length];
+	msort(data,temp,0,data.length-1);
+    }
+    private static void msort(int[] data, int[] temp, int lo, int hi){
+	if (lo != hi){
+	    mid = lo/2 + hi/2;
+	    msort(temp, data, lo, mid);
+	    msort(temp, data, mid+1, hi);
+	}
+	merge(data, temp, lo, mid, high);
+    }
+    
 }
