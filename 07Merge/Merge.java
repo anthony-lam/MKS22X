99f2c85ac4c2b06c4399eaf9372c7095122f9070
Anthony <alam6@stuy.edu>
2018-03-24 11:12:46 -0400

    reverted back to if

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,56 +1,50 @@
 import java.util.*;
 public class Merge{
-    private static void merge (int[] data, int[] temp, int lo, int hi){
-	int mid = (lo + hi) /2 +1;
-	int start = lo;
-	int start2 = mid;
-	int l = lo;
-	while (start < mid && start2<=hi && l <=hi){
-	    System.out.println(Arrays.toString(data));
-	    if (temp[start]<temp[start2]){
-		data[l] = temp[start];
-		start++;
-		l++;
-	    }
-	    else{
-		data[l]= temp[start2];
-		start2++;
-		l++;
-	    }
+	private static void merge (int[] data, int[] temp, int lo, int hi){
+		int mid = (lo+hi)/2+1;
+		int start = lo;
+		int start2 = mid; 
+		for (int l=lo; l<hi; l++){
+			if (start < mid && start2<hi){
+				if (data[start]<data[start2]){
+					temp[l] = data[start];
+					start++;
+				}
+				else{
+					temp[l]= data[start2];
+					start2++;
+				}
+			}
+			else{
+				if (start<mid){
+					temp[l]=data[start];
+					start++;
+				}
+				else{
+					temp[l]=data[start2];
+					start2++;
+				}
+			}
+		}
 	}
-	while (start<mid  && l <=hi){
-	    data[l]=temp[start];
-	    start++;
-	    l++;
+	public static void mergeSort(int[] data){
+		int[] temp = new int[data.length];
+		msort(data,temp,0,data.length-1);
 	}
-	while(start2<=hi && l <=hi){
-	    data[l]=temp[start2];
-	    start2++;
-	    l++;
+	private static void msort(int[] data, int[] temp, int lo, int hi){
+		for(int l=lo;l<hi;l++){
+			temp[l]=data[l];
+		}
+		int mid = (lo+hi)/2;
+		if (hi>lo){
+			msort(temp, data, lo, mid);
+			msort(temp, data, mid+1, hi);
+			merge(data, temp, lo, hi);
+		}
 	}
-    }
-    public static void mergeSort(int[] data){
-	int[] temp = new int[data.length];
-	msort(data,temp,0,data.length-1);
-    }
-    private static void msort(int[] data, int[] temp, int lo, int hi){
-	int mid = (lo+hi/2);
-	if (lo>=hi-2){
-	    for (int i=0; i<temp.length;i++){
-		data[i]=temp[i];
-	    }
-	    return ;
+	public static void main(String[] args) {
+		int[] a = {1,3,5,2};
+		mergeSort(a);
+		System.out.println(Arrays.toString(a));
 	}
-	msort(temp, data, lo, mid);
-	msort(temp, data, mid+1, hi);
-	merge(data, temp, lo, hi);
-	for(int i  = lo;i <= hi;i++){
-	    temp[i] = data[i];
-	}
-    }
-    public static void main(String[] args) {
-	int[] a = {1,3,5,2};
-	mergeSort(a);
-	System.out.println(Arrays.toString(a));
-    }
 }
