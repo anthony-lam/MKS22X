fd157e64a5635fb5b5c4c71c4ff3e6d9f4fd0250
Anthony <alam6@stuy.edu>
2018-03-28 14:43:00 -0400

    created add(value) method

--- 08LinkedLists/MyLinked.java
+++ 08LinkedLists/MyLinked.java
@@ -1,37 +1,62 @@
 public class MyLinked{
-	private Node first;
-	private Node last;
-	private int length;
-	public int size(){
-		return length;
+    private Node first;
+    private Node last;
+    private int length;
+    public int size(){
+	return length;
+    }
+    public int get(int n){
+	int index = 0;
+	Node current = first;
+	while (index != n){
+	    current = current.getNext();
+	    index++;
 	}
-	private class Node{
-		private int data;
-		private Node next;
-		private Node prev;
-		public Node(int data){
-			this.data = data;
-		}
-		public int getValue(){
-			return data;
-		}
-		public Node getNext(){
-			return next;
-		}
-		public Node getPrev(){
-			return prev;
-		}
-				public boolean setValue(int data){
-			this.data=data;
-			return true;
-		}
-		public boolean setNext(Node next){
-			this.next=next;
-			return true;
-		}
-		public boolean setPrev(Node prev){
-			this.prev=prev;
-			return true;
-		}
+	return current.getValue();
+    }
+    public boolean add(int value){
+	Node newNode = new Node(value);
+	last.setNext(newNode);
+	last = last.getNext();
+	length++;
+    }
+    private Node getNode(int index){
+	int n = 
+    public void set(int index, int value){
+    }
+    public void add(int index, int value){
+    }
+    public void remove(int index, int value){
+    }
+    private class Node{
+	private int data;
+	private Node next;
+	private Node prev;
+	public Node(int data){
+	    this.data = data;
 	}
+	public int getValue(){
+	    return data;
+	}
+	public Node getNext(){
+	    return next;
+	}
+	public Node getPrev(){
+	    return prev;
+	}
+	public boolean setValue(int data){
+	    this.data=data;
+	    return true;
+	}
+	public boolean setNext(Node next){
+	    this.next=next;
+	    return true;
+	}
+	public boolean setPrev(Node prev){
+	    this.prev=prev;
+	    return true;
+	}
+	public String toString(){
+	}
+    }
 }
