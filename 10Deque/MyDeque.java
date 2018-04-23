45fa92f0931eaa2b96b687056d16a793a1b510ee
Anthony <alam6@stuy.edu>
2018-04-22 20:43:14 -0400

    finished and tested

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,98 +1,128 @@
 import java.util.*;
 public class MyDeque<T>{
-    private T[] array;
-    private int start;
-    private int end;
-    private int length;
+	private T[] array;
+	public int start;
+	public int end;
+	private int length;
+	public int capacity;
 
-    @SuppressWarnings("unchecked")
-    public MyDeque(){
-	array = (T[]) new Object[10];
-	length = 0;
-	start = 0;
-	end = 5;
-    }
-    @SuppressWarnings("unchecked")
-    public MyDeque(int capacity){
-	if (capacity <0){
-	    throw new IllegalArgumentException();
+	@SuppressWarnings("unchecked")
+	public MyDeque(){
+		array = (T[]) new Object[10];
+		capacity =10;
+		length = 0;
+		start = 4;
+		end = 5;
 	}
-	array = (T[]) new Object[capacity];
-	length = 0;
-	start = 0;
-	end = capacity/2;
-    }
-    public int size(){
-	return length;
-    }
-    public void addFirst(T value){
-	if (value==null){
-	    throw new NullPointerException();
+	@SuppressWarnings("unchecked")
+	public MyDeque(int capacity){
+		if (capacity <0){
+			throw new IllegalArgumentException();
+		}
+		array = (T[]) new Object[capacity];
+		length = 0;
+		end = capacity/2;
+		start = end-1;
+		this.capacity=capacity;
 	}
-	if (start==0){
-	    start = array.length-1;
+	public int size(){
+		return length;
 	}
-	else{
-	    start--;
+	public void addFirst(T value){
+		if (value==null){
+			throw new NullPointerException();
+		}
+		if (array[start]==null){
+			array[start]=value;
+			length++;
+			return ;
+		}
+		resize();
+		if (start==0){
+			start = array.length-1;
+		}
+		else{
+			start--;
+		}
+		array[start]=value;
+		length++;
 	}
-	array[start]=value;
-	length++;
-    }
-    public void addLast(T value){
-	if (value==null){
-	    throw new NullPointerException();
+	public void addLast(T value){
+		if (value==null){
+			throw new NullPointerException();
+		}
+		if (array[end]==null){
+			array[end]=value;
+			length++;
+			return ;
+		}
+		resize();
+		if (end == array.length-1){
+			end=0;
+		}
+		else{
+			end++;
+		}
+		array[end]=value;
+		length++;
 	}
-	if (end == array.length-1){
-	    end=0;
+	public T removeFirst(){
+		if (size()<1){
+			throw new NoSuchElementException();
+		}
+		T ans = array[start];
+		array[start]=null;
+		if (start==capacity-1){
+			start=0;
+		}
+		else{
+			start++;
+		}
+		length--;
+		return ans;
 	}
-	else{
-	    end++;
+	public T removeLast(){
+		if (size()<1){
+			throw new NoSuchElementException();
+		}
+		T ans = array[end];
+		array[end]=null;
+		if (end==0){
+			end=capacity-1;
+		}
+		else{
+			end--;	
+		}
+		length--;
+		return ans;
 	}
-	array[end]=value;
-	length++;
-    }
-    public T removeFirst(){
-	if (size()<1){
-	    throw new NoSuchElementException();
+	public T getFirst(){
+		if (size()<1){
+			throw new NoSuchElementException();
+		}
+		return array[start];
 	}
-	length--;
-	T ans = array[start];
-	array[start]=null;
-	start++;
-	return ans;
-    }
-    public T removeLast(){
-	if (size()<1){
-	    throw new NoSuchElementException();
+	public T getLast(){
+		if (size()<1){
+			throw new NoSuchElementException();
+		}
+		return array[end];
 	}
-	T ans = array[end];
-	array[end]=null;
-	end--;
-	length--;
-	return ans;
-    }
-    public T getFirst(){
-	if (size()<1){
-	    throw new NoSuchElementException();
+	@SuppressWarnings("unchecked")
+	public void resize(){
+		if(length<capacity-1){
+			return;
+		}
+		T[] newSize = (T[]) new Object[capacity*2];
+		int current=start;
+		for (int i = 0; i<length;i++){
+			newSize[i]=array[current%capacity];
+			current++;
+		}
+		array=newSize;
+		start=0;
+		end=length-1;
+		capacity*=2;
 	}
-	return array[start];
-    }
-    public T getLast(){
-	if (size()<1){
-	    throw new NoSuchElementException();
-	}
-	return array[end];
-    }
-    public static void main(String[] args){
-	MyDeque<Integer> a = new MyDeque<>(20);
-	a.addLast(new Integer(45));
-	a.addLast(new Integer(22));
-	a.addLast(new Integer(22));
-	a.addLast(new Integer(4854));
-	for (int i = 0; i<4 ;i++){
-	    System.out.println(a.removeLast ());
-	}
-    }
 }
-    
-    
+
