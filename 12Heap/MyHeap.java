b159b0080f17a7e28db3358c0c86ceab85b0f69f
Anthony <alam6@stuy.edu>
2018-05-01 22:28:54 -0400

    finished heapsort

--- 12Heap/MyHeap.java
+++ 12Heap/MyHeap.java
@@ -1,148 +1,133 @@
 import java.util.*;
 public class MyHeap<T extends Comparable<T>>{
-    T[] heap;
-    int size=10;
-    boolean max = true;
-    int current = 0;
-    @SuppressWarnings("unchecked")
-    public MyHeap(){
-	heap = (T[])new Comparable[10];
-    }
-    @SuppressWarnings("unchecked")
-    public MyHeap(boolean max){
-	this.max = max;
-	heap = (T[])new Comparable[10];
-    }
-    @SuppressWarnings("unchecked")
-    public void resize(){
-	if (current<size-1){
-	    return ;
+	T[] heap;
+	int size=10;
+	boolean max = true;
+	int current = 0;
+	@SuppressWarnings("unchecked")
+	public MyHeap(){
+		heap = (T[])new Comparable[10];
 	}
-	T[] temp = (T[])new Comparable[size*2];
-	for (int i=0;i<size;i++){
-	    temp[i]=heap[i];
+	@SuppressWarnings("unchecked")
+	public MyHeap(boolean max){
+		this.max = max;
+		heap = (T[])new Comparable[10];
 	}
-	size*=2;
-	heap=temp;
-    }
-    public void add(T value){
-	resize();
-	heap[current]=value;
-	if (max){
-	    pushMax(current);
+	@SuppressWarnings("unchecked")
+	public void resize(){
+		if (current<size-1){
+			return ;
+		}
+		T[] temp = (T[])new Comparable[size*2];
+		for (int i=0;i<size;i++){
+			temp[i]=heap[i];
+		}
+		size*=2;
+		heap=temp;
 	}
-	else{
-	    pushMin(current);
+	public void add(T value){
+		resize();
+		heap[current]=value;
+		if (max){
+			pushMax(current);
+		}
+		else{
+			pushMin(current);
+		}
+		current++;
 	}
-	current++;
-    }
-    public void pushMax(int index){
-	int parent = (index-1)/2;
-	if (parent<=0||heap[index].compareTo(heap[parent])<0){
-	    return ;
+	public void pushMax(int index){
+		int parent = (index-1)/2;
+		if (index==0||parent<0||heap[index].compareTo(heap[parent])<0){
+			return ;
+		}
+		T temp = heap[parent];
+		heap[parent]=heap[index];
+		heap[index]=temp;
+		pushMax(parent);
 	}
-	T temp = heap[parent];
-	heap[parent]=heap[index];
-	heap[index]=temp;
-	pushMax(parent);
-    }
-    private void pushMin(int index){
-	int parent = (index-1)/2;
-	if (parent<=0||heap[index].compareTo(heap[parent])>0){
-	    return ;
+	private void pushMin(int index){
+		int parent = (index-1)/2;
+		if (index==0||parent<0||heap[index].compareTo(heap[parent])>0){
+			return ;
+		}
+		T temp = heap[parent];
+		heap[parent]=heap[index];
+		heap[index]=temp;
+		pushMax(parent);
 	}
-	T temp = heap[parent];
-	heap[parent]=heap[index];
-	heap[index]=temp;
-	pushMax(parent);
-    }
-    public void downMax(int index){
-	int left = index*2+1;
-	int right = left +1;
-	if (left>=current){
-	    return ;
+	public void downMax(int index){
+		int left = index*2+1;
+		int right = left +1;
+		if (left>=current){
+			return ;
+		}
+		if (right>=current && heap[left].compareTo(heap[index])>0){
+			T temp = heap[left];
+			heap[left]=heap[index];
+			heap[index]=temp;
+			downMax(left);
+			return ;
+		}
+		if (heap[left].compareTo(heap[right])>0){
+			T temp = heap[left];
+			heap[left]=heap[index];
+			heap[index]=temp;
+			downMax(left);
+		}
+		else{
+			T temp = heap[right];
+			heap[right]=heap[index];
+			heap[index]=temp;
+			downMax(right);
+		}
 	}
-	if (heap[right]==null&&heap[left].compareTo(heap[index])>0){
-	    T temp = heap[left];
-	    heap[left]=heap[index];
-	    heap[index]=temp;
-	    downMax(left);
-	    return ;
+	public void downMin(int index){
+		int left = index*2+1;
+		int right = left +1;
+		if (left>=current){
+			return ;
+		}
+		if (right>=current && heap[left].compareTo(heap[index])<0){
+			T temp = heap[left];
+			heap[left]=heap[index];
+			heap[index]=temp;
+			downMax(left);
+			return ;
+		}
+		if (heap[left].compareTo(heap[right])<0){
+			T temp = heap[left];
+			heap[left]=heap[index];
+			heap[index]=temp;
+			downMax(left);
+		}
+		else{
+			T temp = heap[right];
+			heap[right]=heap[index];
+			heap[index]=temp;
+			downMax(right);
+		}
 	}
-	if (heap[right]==null){
-	    return ;
+	public T remove(){
+		System.out.println(Arrays.toString(heap));
+		T ans = heap[0];
+		heap[0]=heap[current-1];
+		if (max){
+			downMax(0);
+		}
+		else{
+			downMin(0);
+		}
+		current--;
+		return ans;
 	}
-	if (heap[index].compareTo(heap[left])<0 && heap[index].compareTo(heap[right])<0){
-	    return ;
+	public T peek(){
+		return heap[0];
 	}
-	if (heap[left].compareTo(heap[right])>0){
-	    T temp = heap[left];
-	    heap[left]=heap[index];
-	    heap[index]=temp;
-	    downMax(left);
-	    return ;
+	public int size(){
+		return current;
 	}
-	else{
-	    T temp = heap[right];
-	    heap[right]=heap[index];
-	    heap[index]=temp;
-	    downMax(right);
-	    return ;
+	public String toString(){
+		return Arrays.toString(heap);
 	}
-    }
-    private void downMin(int index){
-	int left = index*2+1;
-	int right = left +1;
-	if (left>current){
-	    return ;
-	}
-	if (heap[right]==null&&heap[left].compareTo(heap[index])<0){
-	    T temp = heap[left];
-	    heap[left]=heap[index];
-	    heap[index]=temp;
-	    downMax(left);
-	    return ;
-	}
-	if (heap[right]==null){
-	    return ;
-	}
-	if (heap[index].compareTo(heap[left])>0 && heap[index].compareTo(heap[right])>0){
-	    return ;
-	}
-	if (heap[left].compareTo(heap[right])<0){
-	    T temp = heap[left];
-	    heap[left]=heap[index];
-	    heap[index]=temp;
-	    downMax(left);
-	    return ;
-	}
-	else{
-	    T temp = heap[right];
-	    heap[right]=heap[index];
-	    heap[index]=temp;
-	    downMax(right);
-	    return ;
-	}
-    }
-    public T remove(){
-	T ans = heap[0];
-	heap[0]=heap[current-1];
-	if (max){
-	    downMax(0);
-	}
-	else{
-	    downMin(0);
-	}
-	current--;
-	return ans;
-    }
-    public T peek(){
-	return heap[0];
-    }
-    public int size(){
-	return current;
-    }
-    public String toString(){
-	return Arrays.toString(heap);
-    }
 }
--- 12Heap/Sort.java
+++ 12Heap/Sort.java
@@ -1,17 +1,16 @@
 import java.util.*;
 public class Sort<T extends Comparable<T>>{
     public static void heapSort(Integer[] data){
-	MyHeap<Integer> a = new MyHeap<>(true);
+	MyHeap<Integer> a = new MyHeap<>();
 	for (int i=0;i<data.length;i++){
 	    a.add(data[i]);
 	}
-	System.out.println(a.toString());
 	while (a.size()>0){
 	    data[a.size()-1]=a.remove();
 	}
     }
     public static void main(String[] args){
-	Integer[] a ={1,54,23,3,53};
+	Integer[] a ={1,54,23,3,53,23,455,234,31};
 	heapSort(a);
 	System.out.println(Arrays.toString(a));
     }
